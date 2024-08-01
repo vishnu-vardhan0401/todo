12d85ddb7d2fcf2e4e66861f4ea8e96738340f59
vishnu-vardhan0401 <147438443+vishnu-vardhan0401@users.noreply.github.com>
2024-08-01 21:46:09 +0530

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -33,7 +33,7 @@ function Todo() {
 
   const handleAdd = async () => {
     if (title === '') {
-     
+      toast.error('Title cannot be empty');
       return;
     }
 
@@ -45,9 +45,9 @@ function Todo() {
       setTitle('');
       retrieveTodos();
       setEdit(false);
-      
+      toast.success('Todo added successfully');
     } catch (e) {
-      console.log(e.message);
+      toast.error(e.message);
     }
   };
 
@@ -56,9 +56,9 @@ function Todo() {
       const docRef = doc(db, 'books', id);
       await deleteDoc(docRef);
       retrieveTodos();
-      
+      toast.success('Todo deleted successfully');
     } catch (e) {
-     console.log(e.message);
+      toast.error(e.message);
     }
   };
 
@@ -68,7 +68,7 @@ function Todo() {
       const todos = snap.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
       setTodos(todos);
     } catch (e) {
-     console.log(e)
+      toast.error(e.message);
     }
   };
 
@@ -86,7 +86,7 @@ function Todo() {
         }
       }
     } catch (e) {
-      console.log(e.message);
+      toast.error(e.message);
     }
   };
 
@@ -98,37 +98,37 @@ function Todo() {
       })
     }
     catch(e){
-      console.log(e.message)
+      toast.error(e.message)
     }
     }
 
   return (
-    <div className='w-[100%] border-2 border-black  flex flex-col gap-2 items-center  mt-4 h-full'>
-      <h1>Todo</h1>
-      <div className='flex  gap-5 w-[100vw] items-center justify-center  '>
+    <div className=' w-[90%] ml-6 px-4 md:w-[70%] lg:w-[70%] bg-gray-100 flex flex-col  gap-2 items-center  lg:mt-10 lg:ml-20 md:mt-10 md:ml-20 h-full shadow-2xl rounded-3xl pb-5'>
+      <h1 className=' text-blue-500 text-3xl'>MY Todo-s</h1>
+      <div className='flex  gap-5 w-[100vw] items-center justify-center'>
         <input
-          className='border-2 border-black w-52'
+          className='border-2 p-1.5 w-64 md:min-w-[470px] lg:min-w-[500px]'
           type='text'
           name='title'
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
-        <button className='border-2 border-black' onClick={handleAdd}>
+        <button className=' p-2 px-6 rounded-xl bg-blue-400 text-white' onClick={handleAdd}>
           Add
         </button>
       </div>
       {todos.map((item) => (
         <div className='flex gap-2 my-2' key={item.id}>
-          <h1 className='border-2 border-black break-words w-52 p-1'>{item.title}</h1>
+          <h1 className=' break-words w-52 p-1 pl-4 '>{item.title}</h1>
           <button
-            className='border-2 border-black p-1'
+            className=' border-2 px-4 py-2'
             disabled={edit}
             onClick={() => handleEdit(item.id)}
           >
             Edit
           </button>
           <button
-            className='border-2 border-black p-1'
+            className=' border-2 px-4'
             disabled={edit}
             onClick={() => deleteTodo(item.id)}
           >
