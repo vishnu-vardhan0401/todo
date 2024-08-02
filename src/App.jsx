3ca2b559b1e9317b50611e1439519a578375509b
vishnu-vardhan0401 <147438443+vishnu-vardhan0401@users.noreply.github.com>
2024-08-02 13:28:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import Signup from './components/Signup'
 import Todo from './components/Todo'
 import './index.css'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 function App() {
 
   return (
     <>
+      <ToastContainer/>
    <BrowserRouter>
    <Routes>
     <Route path="/" element={<Signup />} />
